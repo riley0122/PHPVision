7a42e1f490bc8f9783504c367856287ebee87fe8
Riley0122 <77288737+riley0122@users.noreply.github.com>
2024-08-26 00:42:02 +0200

    Example page

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require "phpVisionApi.php";
+register_page_hit("/");
+register_active_user();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A quick example page</title>
+</head>
+<body>
+    <h1>:)</h1>
+    <p>This is an example page for PHPVision. This page should not be on your website lol.</p>
+</body>
+</html>
