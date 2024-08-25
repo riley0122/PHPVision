804ecc049dd4b04d809382077b5eea14c619b393
Riley0122 <77288737+riley0122@users.noreply.github.com>
2024-08-25 23:21:38 +0200

    Add custom event register

--- phpVisionApi.php
+++ phpVisionApi.php
@@ -3,7 +3,7 @@ require "phpVisionConfig.php";
 
 function register_active_user() {
     if(session_status() !== PHP_SESSION_ACTIVE) session_start();
-    if ($_SESSION["phpvision_has_registered"]) return;
+    if (isset($_SESSION["phpvision_has_registered"]) && $_SESSION["phpvision_has_registered"]) return;
     $_SESSION["phpvision_has_registered"] = true;
     $conn = new mysqli($GLOBALS["db_server_name"], $GLOBALS["db_username"], $GLOBALS["db_password"]);
     mysqli_select_db($conn, "phpVision");
@@ -20,6 +20,17 @@ function register_page_hit($path) {
     $stmt = $conn->prepare("INSERT INTO Events (Time, type, data) VALUES (NOW(), ?, ?)");
     $type = "pageView";
     $data = json_encode(array("page" => $path));
+    $stmt->bind_param("ss", $type, $data);
+    $stmt->execute();
+    $conn->close();
+}
+
+function register_custom_event($name, $info="") {
+    $conn = new mysqli($GLOBALS["db_server_name"], $GLOBALS["db_username"], $GLOBALS["db_password"]);
+    mysqli_select_db($conn, "phpVision");
+    $stmt = $conn->prepare("INSERT INTO Events (Time, type, data) VALUES (NOW(), ?, ?)");
+    $type = "custom";
+    $data = json_encode(array("name" => $name, "info" => $info));
     $stmt->bind_param("ss", $type, $data);
     $stmt->execute();
     $conn->close();
